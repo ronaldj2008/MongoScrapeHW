b17e756bffd5399659a09aba243d3b8a2d5e435f
Ron J <ronaldj2008@gmail.com>
2017-06-07 15:57:03 -0700

    Set up server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+var request = require("request");
+var cheerio = require("cheerio");
