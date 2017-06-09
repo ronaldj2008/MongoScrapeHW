067aac3a2dca4a543314916ae9fc67d17440a4af
Ron J <ronaldj2008@gmail.com>
2017-06-08 18:06:13 -0700

    Added scraping info to server need to test before graded

--- server.js
+++ server.js
@@ -5,3 +5,33 @@ var mongoose = require("mongoose");
 
 var request = require("request");
 var cheerio = require("cheerio");
+
+var app = express();
+
+app.use(express.static("public"));
+
+request('https://medium.com/topic/entrepreneurship', function (error, response, html) {
+
+	// Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  var $ = cheerio.load(html);
+
+  // An empty array to save the data that we'll scrape
+  var result = [];
+
+  // Select each instance of the HTML body that you want to scrape
+  // NOTE: Cheerio selectors function similarly to jQuery's selectors, 
+  // but be sure to visit the package's npm page to see how it works
+  $('.js-topicStream').each(function(i, element){
+
+    var link = $(element).children().attr("href");
+    var title = $(element).children().text();
+
+    // Save these results in an object that we'll push into the result array we defined earlier
+    result.push({
+      title: title,
+      link: link
+    });
+    });
+  console.log(result);
+});
